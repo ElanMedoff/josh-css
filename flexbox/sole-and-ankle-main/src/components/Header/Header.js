ecb8dc73d106012cba42a6e874f781fb92cdde4e
Elan <54599408+ElanMedoff@users.noreply.github.com>
2021-07-04 17:11:24 -0600

    flexbox project

---
+++ flexbox/sole-and-ankle-main/src/components/Header/Header.js
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+import { COLORS, WEIGHTS } from "../../constants";
+import Logo from "../Logo";
+import SuperHeader from "../SuperHeader";
+
+const Header = () => {
+  // Our site features two visual headers, but they should be
+  // grouped semantically as a single header.
+  return (
+    <header>
+      <SuperHeader />
+      <MainHeader>
+        <Logo />
+        <Nav>
+          <NavLink href="/sale">Sale</NavLink>
+          <NavLink href="/new">New&nbsp;Releases</NavLink>
+          <NavLink href="/men">Men</NavLink>
+          <NavLink href="/women">Women</NavLink>
+          <NavLink href="/kids">Kids</NavLink>
+          <NavLink href="/collections">Collections</NavLink>
+        </Nav>
+      </MainHeader>
+    </header>
+  );
+};
+
+const MainHeader = styled.div`
+  padding: 16px;
+  border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  gap: 16px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  gap: 32px;
+  margin: auto;
+`;
+
+const NavLink = styled.a`
+  font-size: 1.125rem;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: ${COLORS.gray[900]};
+  font-weight: ${WEIGHTS.medium};
+
+  &:first-of-type {
+    color: ${COLORS.secondary};
+  }
+`;
+
+export default Header;
---
+++ flexbox/sole-and-ankle-main/src/components/ShoeGrid/ShoeGrid.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+import SHOES from "../../data";
+import ShoeCard from "../ShoeCard";
+
+const ShoeGrid = () => {
+  return (
+    <Wrapper>
+      {SHOES.map((shoe, index) => (
+        <ShoeCard key={shoe.slug} {...shoe} />
+      ))}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 32px;
+`;
+
+export default ShoeGrid;
